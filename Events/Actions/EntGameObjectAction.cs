a0016734668e7779ec4dc12bc997a5118b0ef4ce
Chris McMath <chrismcmath@gmail.com>
2016-01-23 22:12:04 +0000

    Fix some Action issues
    -- No need for Serializable (they're Monobehaviours)
    -- Bizarre bug in Unity allows adding a component with filename and class name differing; it'll break after Play

--- Events/Actions/EntGameObjectAction.cs
+++ Events/Actions/EntGameObjectAction.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace Ent2D.Events.Actions {
-    [System.Serializable]
     public class EntGameObjectAction : EntAction {
         public bool Active;
 
